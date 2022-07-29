b509320fad892b6016cd9a8e6bf902ed549bec82
Thanuchcha1603 <44134629+Thanuchcha1603@users.noreply.github.com>
2022-07-29 13:38:03 +0700

    add structure

---
+++ app/Views/index.php
@@ -0,0 +1,318 @@
+<?php
+APPPATH . ".style.css";
+APPPATH . ".script.js";
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+  <title>Downright Bossy Hyena</title>
+  <meta property="og:title" content="Downright Bossy Hyena" />
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="utf-8" />
+  <meta property="twitter:card" content="summary_large_image" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('.style.css')?>">
+
+  <style data-tag="reset-style-sheet">
+    html {
+      line-height: 1.15;
+    }
+
+    body {
+      margin: 0;
+    }
+
+    * {
+      box-sizing: border-box;
+      border-width: 0;
+      border-style: solid;
+    }
+
+    p,
+    li,
+    ul,
+    pre,
+    div,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      margin: 0;
+      padding: 0;
+    }
+
+    button,
+    input,
+    optgroup,
+    select,
+    textarea {
+      font-family: inherit;
+      font-size: 100%;
+      line-height: 1.15;
+      margin: 0;
+    }
+
+    button,
+    select {
+      text-transform: none;
+    }
+
+    button,
+    [type="button"],
+    [type="reset"],
+    [type="submit"] {
+      -webkit-appearance: button;
+    }
+
+    button::-moz-focus-inner,
+    [type="button"]::-moz-focus-inner,
+    [type="reset"]::-moz-focus-inner,
+    [type="submit"]::-moz-focus-inner {
+      border-style: none;
+      padding: 0;
+    }
+
+    button:-moz-focus,
+    [type="button"]:-moz-focus,
+    [type="reset"]:-moz-focus,
+    [type="submit"]:-moz-focus {
+      outline: 1px dotted ButtonText;
+    }
+
+    a {
+      color: inherit;
+      text-decoration: inherit;
+    }
+
+    input {
+      padding: 2px 4px;
+    }
+
+    img {
+      display: block;
+    }
+
+    html {
+      scroll-behavior: smooth
+    }
+  </style>
+  <style data-tag="default-style-sheet">
+    html {
+      font-family: Inter;
+      font-size: 16px;
+    }
+
+    body {
+      font-weight: 400;
+      font-style: normal;
+      text-decoration: none;
+      text-transform: none;
+      letter-spacing: normal;
+      line-height: 1.15;
+      color: var(--dl-color-gray-black);
+      background-color: var(--dl-color-gray-white);
+
+    }
+  </style>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" data-tag="font" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" data-tag="font" />
+</head>
+
+<body>
+  <div>
+    <link href="./home.css" rel="stylesheet" />
+
+    <div class="home-container">
+      <div class="home-desktop2">
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/5f96c873-6d44-4ce2-9587-04d4b655ce26?org_if_sml=11691400" alt="02401311" class="home-image02" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/2ec8ca2e-0a1c-4592-8d5e-cd1a3e4c0b53?org_if_sml=12819435" alt="01401312" class="home-image01" />
+        <img src="public/playground_assets/e1c7316d-6ba2-4665-93ef-4f81e8c6c3db-rikl.svg" alt="Rectangle5401313" class="home-rectangle5" />
+        <span class="home-text">
+          <span class="home-text01">
+            <span>เงื่อนไขในการรับรางวัล</span>
+            <br />
+            <span></span>
+          </span>
+          <span class="home-text05">
+            <span>
+              1. ผู้ร่วมกิจกรรมสามารถลงทะเบียนได้ 1 คนต่อ 1 สิทธิ์เท่านั้น
+            </span>
+            <br />
+            <span>
+              2. บริษัท เพ็ท โฟกัส จำกัด (“บริษัทฯ”)
+              ขอสงวนสิทธิ์ในการพิจารณายกเลิกสิทธิ์ของผู้ร่วมกิจกรรม
+              หากไม่ปฏิบัติตามกฎกติกามีการทุจริต
+              หรือส่อเจตนาทุจริตในการร่วมกิจกรรม หรือรับรางวัลใด ๆ
+            </span>
+            <br />
+            <span>
+              หากแม้ตรวจสอบพบในภายหลัง
+              ให้มีผลย้อนหลังในการยกเลิกสิทธิ์ออกจากการเข้าร่วมกิจกรรม
+            </span>
+            <br />
+            <span>
+              3. ของรางวัลไม่สามารถเปลี่ยนเป็นเงินสด หรือรางวัลอื่น ๆ ได้
+            </span>
+            <br />
+            <span>
+              4.
+              ผู้ร่วมกิจกรรมจะได้รับสินค้าขนาดทดลองก็ต่อเมื่อท่านเป็นผู้ร่วมกิจกรรมที่กรอกข้อมูลครบถ้วน
+              20,000 คนแรก พร้อมทั้งได้ยอมรับและตกลงตามข้อตกลง
+              และเงื่อนไขการรับสินค้าขนาดทดลอง
+            </span>
+            <br />
+            <span>
+              ฉบับนี้แล้วเท่านั้น บริษัทฯ ขอสงวนสิทธิในการแก้ไขเพิ่มเติม
+              ข้อตกลงและเงื่อนไขการรับสินค้าขนาดทดลองฉบับนี้ได้ตลอดเวลา
+              โดยไม่จำเป็นต้องบอกกล่าวล่วงหน้า
+            </span>
+            <br />
+            <span>
+              5. สงวนสิทธิ์การเข้าร่วมรายการเฉพาะผู้ที่ถือสัญชาติไทย
+              และมีภูมิลำเนาอยู่ในราชอาณาจักรไทยเท่านั้น
+            </span>
+            <br />
+            <span>
+              6. หากผู้ร่วมกิจกรรมไม่ครบตามจำนวนของรางวัลที่กำหนด
+              ขออนุญาตตัดสิทธิ์แจกรางวัลตามจำนวนผู้ร่วมกิจกรรม
+              และขอสงวนสิทธิ์แจกของรางวัลเฉพาะผู้ร่วมกิจกรรมเท่านั้น
+            </span>
+            <br />
+            <span>
+              7. โดยการลงทะเบียน ตอบแบบสอบถามและกรอกข้อมูลทั้งหมดในกิจกรรมนี้
+              ผู้ลงทะเบียนทุกท่านอนุญาตและยิมยอมให้ทีมงานของบริษัท เพ็ท โฟกัส
+              จำกัด และตัวแทน จัดเก็บ รวบรวม ใช้ เผยแพร่
+            </span>
+            <br />
+            <span>
+              ชื่อ-นามสกุล รูปภาพ
+              และรายละเอียดอื่นใดของผู้ร่วมกิจกรรมที่ให้ไว้ ณ ที่นี้
+              เพื่อวิเคราะห์และพัฒนาผลิตภัณฑ์ตลอดจนเพื่อการประชาสัมพันธ์ได้ในสื่อทุกชนิด
+            </span>
+            <br />
+            <span>
+              8. บริษัทฯ
+              มีนโยบายคุ้มครองข้อมูลส่วนบุคคลที่อ่อนไหวที่ท่านได้ให้ไว้
+              เพื่อวัตถุประสงค์ในการเสนอสินค้าขนาดทดลองเท่านั้น บริษัทฯ
+              จะไม่เก็บรวบรวม ใช้ หรือ เปิดเผยข้อมูลส่วนบุคคลที่อ่อนไหว
+            </span>
+            <br />
+            <span>
+              ให้แก่ผู้ใดหรือเพื่อประโยชน์อื่นใดอีก นอกเหนือจาก
+              วัตถุประสงค์อันเป็นการเฉพาะในข้อตกลงและเงื่อนไขการรับสินค้าขนาดทดลองฉบับนี้
+              เท่านั้น
+            </span>
+          </span>
+        </span>
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/98c48820-9f64-455d-85af-cee809973f29?org_if_sml=1154885" alt="C00322ACADC543F1A082910BEB985882PailinMuangdee1401315" class="home-c00322a-c-a-d-c543f1a082910b-e-b985882pailin-muangdee1" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/ec8b716a-0902-48b0-9982-399ef91cdba1?org_if_sml=1149033" alt="Screenshot20220610145313comfacebook1401316" class="home-screenshot20220610145313comfacebook1" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/26dc099c-b58e-4797-9ab4-d01102e8c63d?org_if_sml=1173078" alt="RujeeOusdinsak1401317" class="home-rujee-ousdinsak1" />
+        <img src="public/playground_assets/b27fc960-94f5-41a6-87ee-c04a091333d3-vvlo.svg" alt="Rectangle1401318" class="home-rectangle1" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/050eaed2-1177-47cc-ac13-0900d8bd61f1?org_if_sml=14565" alt="logobetagro031401319" class="home-logobetagro031" />
+        <span class="home-text29">รายละเอียดสินค้า</span>
+        <span class="home-text30">รีวิวสินค้า</span>
+        <span class="home-text31">รีวิวสินค้า</span>
+        <span class="home-text32">ขั้นตอนการลงทะเบียน</span>
+        <span class="home-text33">
+          <span>Video</span>
+          <br />
+          <span>Q&amp;A</span>
+        </span>
+        <span class="home-text37">
+          ผลิตภัณฑ์ อาหารสุนัข DOG n joy ขนาด 100 กรัม 1 ชิ้น จำนวน 20,000
+          รางวัล
+        </span>
+        <span class="home-text38">
+          <span>ตอบทุกข้อสงสัยกับอาหาร</span>
+          <br />
+          <span>น้องหมา น้องแมว สูตรทำหมัน</span>
+        </span>
+        <span class="home-text42">ยินยอม</span>
+        <span class="home-text43">
+          Capture ภาพความน่ารักของน้องหมา น้องแมว
+          กับอาหารสูตรทำหมันที่ท่านได้แชร์ลงบน Social Media
+        </span>
+        <span class="home-text44">
+          <span class="home-text45">
+            <span>ระยะเวลากิจกรรม</span>
+            <br />
+            <span></span>
+          </span>
+          <span class="home-text49">
+            <span>
+              ตั้งแต่วันที่ 18 กรกฎาคม 2565 จนถึงวันที่ 14 สิงหาคม 2565 เวลา
+              23:59 น.
+            </span>
+            <br />
+            <span>
+              * สงวนสิทธิ์ 1 ท่าน ต่อ 1 สิทธิ์ในการลงทะเบียนรับผลิตภัณฑ์
+              โดยต้องลงทะเบียนในระยะเวลาที่กำหนดเท่านั้น
+              ทั้งนี้ผู้เข้าร่วมกิจกรรมต้องทำตามกติกาอย่างครบถ้วนเท่านั้น
+            </span>
+            <br />
+            <span>
+              หากทำตามกติกาไม่ครบ
+              ไม่ว่ากรณีใดถือว่าเป็นการลงทะเบียนดังกล่าวเป็นโมฆะ
+            </span>
+          </span>
+        </span>
+        <span class="home-text55">ลงทะเบียน</span>
+        <span class="home-text56">ร้านค้า</span>
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/c5df3a30-7043-48b8-8cd2-20f52728d6b1?org_if_sml=1176242" alt="Ab25Cc1401332" class="home-ab25cc1" />
+        <img src="public/playground_assets/2eef4c73-97c7-4b78-937a-f91668e85a17-8udo.svg" alt="Rectangle3401333" class="home-rectangle3" />
+        <img src="public/playground_assets/5ec9844d-1a43-403e-932c-c1a758225c84-kz1f.svg" alt="Rectangle22401334" class="home-rectangle22" />
+        <img alt="Vector5401335" src="public/playground_assets/95aa08d1-4618-41ad-8644-1d8260bb5a26-96il.svg" class="home-svg" />
+        <img alt="Vector1401336" src="public/playground_assets/6e22ec1f-f927-4479-be0a-abe1444c6b5b-fkk7.svg" class="home-svg01" />
+        <img alt="Vector9401337" src="public/playground_assets/5dfcd5cf-e446-43d3-91b3-2bd2477fdb68-9ckc.svg" class="home-svg02" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/dfa79e66-f1db-4a68-9986-3b8cfa3b8175?org_if_sml=165297" alt="CNJNeuteredMockup100gfront1401338" class="home-c-n-j-neutered-mockup100gfront1" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/4ec74eb0-08de-41a6-b1f8-b27cea1a83ae?org_if_sml=11973" alt="element015401339" class="home-element015" />
+        <span class="home-text57">ค้นหาอาหารสูตรทำหมันใกล้บ้านคุณ</span>
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/f685bdd9-4c96-4ad4-9b5d-97b66f5576d5?org_if_sml=11742" alt="element011401341" class="home-element011" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/58fdca3d-461a-4884-8292-d3115fbf06ba?org_if_sml=11966" alt="element013401342" class="home-element013" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/b1d6086a-f338-4f89-a426-0641568ccfa6?org_if_sml=12968" alt="element014401343" class="home-element014" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/2b80cbf3-9166-47c1-bfe5-14fbf3c26d84?org_if_sml=11337" alt="element012401344" class="home-element012" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/7c6a3289-c7b2-4daa-aeb2-8b1be2559142?org_if_sml=161226" alt="DNJNeuteredMockup100gfront1401345" class="home-d-n-j-neutered-mockup100gfront1" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/5a8125da-84ee-4ee4-b399-c1f8bc9cea5e?org_if_sml=1412749" alt="ScreenShot20220726at3201401346" class="home-screen-shot20220726at3201" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/c18c2b1b-35dc-493f-a53c-df4ae4214408?org_if_sml=12569" alt="element021401347" class="home-element021" />
+        <span class="home-text58">ยินยอม</span>
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/80a82788-bef5-4324-a6b2-d6f1dbf8f50e?org_if_sml=1821" alt="iconsocial02401349" class="home-iconsocial02" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/b82fa272-59ec-41d1-a3a5-ca50a30ec6c3?org_if_sml=11216" alt="iconsocial03401350" class="home-iconsocial03" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/b05119ce-5ae3-4ace-a5d3-c5ef38f509ed?org_if_sml=11068" alt="iconsocial04401351" class="home-iconsocial04" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/b844e8e1-0e30-4042-9875-b214b396fa49?org_if_sml=11747" alt="element03021401352" class="home-element03021" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/745638d1-913e-43dc-be0c-626bc4e4de6a?org_if_sml=11223" alt="element03025401353" class="home-element03025" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/868836d7-421f-4f35-ab3e-5fb71a147ba7?org_if_sml=11160" alt="element03024401354" class="home-element03024" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/f111c170-0e6a-4c53-b509-a6ba465354be?org_if_sml=11697" alt="element03023401355" class="home-element03023" />
+        <img src="public/playground_assets/bd0566f4-981e-43ef-b8d3-746ad76e8ee2-0d48.svg" alt="Rectangle16401356" class="home-rectangle16" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/b61f0b39-291e-479a-9f93-e015f2661c12?org_if_sml=12378" alt="element03022401357" class="home-element03022" />
+        <img alt="Vector6401358" src="public/playground_assets/5fd9f002-4019-49ba-86c8-f569fe80731d-cet.svg" class="home-svg03" />
+        <img src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/60fa648f-2227-497b-b4fe-e4dae68d2b01/c8d75cfc-75e9-4d19-bc1e-101ab2981ce4?org_if_sml=1123" alt="Rectangle17401359" class="home-rectangle17" />
+        <img src="public/playground_assets/d040450a-9742-4f25-b4ef-27439f5f7011-i7w.svg" alt="Rectangle8401360" class="home-rectangle8" />
+        <img src="public/playground_assets/560c5c6c-d086-4c11-8ffd-3f876d42793e-7ic.svg" alt="Rectangle18401361" class="home-rectangle18" />
+        <img src="public/playground_assets/a2b98f08-6713-417d-a816-95fe470680e2-98l2.svg" alt="Rectangle20401362" class="home-rectangle20" />
+        <img src="public/playground_assets/67df0082-4005-40f4-94f8-209514545248-41ja.svg" alt="Rectangle19401363" class="home-rectangle19" />
+        <img src="public/playground_assets/3a6c4c86-dc75-4c29-b5de-b4bc38062865-1m5m.svg" alt="Rectangle21401364" class="home-rectangle21" />
+        <span class="home-text59">ถัดไป</span>
+        <span class="home-text60">ภูมิภาค</span>
+        <span class="home-text61">จังหวัด</span>
+        <span class="home-text62">ไป</span>
+        <span class="home-text63">ไป</span>
+        <img alt="Vector7401370" src="public/playground_assets/5842da70-11c2-46b1-8523-1873fb6f2fa9-m19n.svg" class="home-svg04" />
+        <img alt="Vector8401371" src="public/playground_assets/60d3788c-dd1f-4867-b3ef-b7331734758c-o0x.svg" class="home-svg05" />
+        <img alt="Ellipse1401372" src="public/playground_assets/b1da236f-0a6a-43d9-9afb-26be0367ee79-qq9.svg" class="home-svg06" />
+        <img alt="Ellipse2401373" src="public/playground_assets/a147c2ad-e210-4503-9753-152a4b7d8593-ro1.svg" class="home-svg07" />
+        <img alt="Ellipse3401374" src="public/playground_assets/9feeb8ff-6e54-4ee0-9fda-b1536b0a3ff1-y48.svg" class="home-svg08" />
+        <img alt="Ellipse4401375" src="public/playground_assets/191f472d-5675-4492-b11a-514fdee0545d-d36p.svg" class="home-svg09" />
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
